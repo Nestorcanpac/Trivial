6cab3ddda77e7d7a4e8bd445bb77ccd415a3237d
NestorCP <nescanpac@alu.edu.gva.es>
2026-02-26 22:20:26 +0100

    mm

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-// IMPORTANT: Change '/trivial/' to match your GitHub repository name
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/trivial/',
+  base: '/Trivial/',
 })
